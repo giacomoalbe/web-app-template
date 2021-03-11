1ae94eb5a8c8f0be0809a79fa3d282cc3d038e11
Giacomo Alberini <giacomoalbe@gmail.com>
2021-03-11 12:38:38 +0100

    Add env.example configuration

--- create_template.sh
+++ create_template.sh
@@ -14,4 +14,7 @@ source config
 echo "Creating docker-compose.yml..."
 envsub --syntax handlebars docker-compose.template.yml docker-compose.yml
 
+echo "Creating server/.env..."
+envsub --syntax handlebars server/env.example server/.env  
+
 echo "All files created!"
